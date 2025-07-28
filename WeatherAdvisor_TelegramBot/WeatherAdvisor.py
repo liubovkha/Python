9824cea44a78788c33769e74b98551b15f27f5b4
unknown <lyubovechek24@gmail.com>
2025-07-28 17:18:36 +0300

    Telegram bot added

---
+++ WeatherAdvisor_TelegramBot/WeatherAdvisor.py
@@ -0,0 +1,63 @@
+from telebot import TeleBot
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+import credentials
+import requests
+
+TOKEN = f"{credentials.TOKEN}"
+API_KEY = f"{credentials.WEATHER_API_KEY}"
+
+CITY_NAME_TO_CHOOSE = {
+    "London": "London,UK",
+    "Paris": "Paris,France",
+    "Helsinki": "Helsinki,Finland"
+}
+
+DATE_TO_CHOOSE = {
+    "Today": "2025-07-28",
+    "Tomorrow": "2025-07-29",
+    "The day after tomorrow": "2025-07-30"
+}
+
+user_city_map = {}
+
+bot = TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    markup = ReplyKeyboardMarkup(row_width=3)
+    for city_name in CITY_NAME_TO_CHOOSE.keys():
+        item_button = KeyboardButton(city_name)
+        markup.add(item_button)
+    bot.send_message(message.chat.id, "Where are you?", reply_markup=markup)
+
+@bot.message_handler(func=lambda message: message.text in CITY_NAME_TO_CHOOSE.keys())
+def send_date_choice(message):
+    user_id = message.from_user.id
+    user_city_map[user_id] = CITY_NAME_TO_CHOOSE[message.text]
+
+    markup = ReplyKeyboardMarkup(row_width=3)
+    for weather_date in DATE_TO_CHOOSE.keys():
+        item_button = KeyboardButton(weather_date)
+        markup.add(item_button)
+    bot.send_message(message.chat.id, "When do you plan to go outside?", reply_markup=markup)
+
+@bot.message_handler(func=lambda message: message.text in DATE_TO_CHOOSE.keys())
+def send_advice(message):
+    user_id = message.from_user.id
+    city = user_city_map.get(user_id)
+
+    weather_date = message.text
+    date = DATE_TO_CHOOSE[weather_date]
+    weather, description = get_weather_by_hours_for_day_from_api(date=date, city=city)
+    bot.send_message(message.chat.id, text=f"{description}")
+
+def get_weather_by_hours_for_day_from_api(*, date: str, city: str) -> list[dict]:
+    url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{city}/{date}/{date}?key={API_KEY}"
+    response = requests.get(url)
+    weather_by_days = response.json()["days"]
+    weather_description = response.json()["description"]
+    print(weather_by_days)
+    return weather_by_days, weather_description
+
+
+bot.infinity_polling()
